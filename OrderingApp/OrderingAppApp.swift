ac677485935e306561afd7160889ca241d3df96a
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2023-04-05 19:51:28 +0300

    Initial Commit

---
+++ OrderingApp/OrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderingAppApp.swift
+//  OrderingApp
+//
+//  Created by Collins Muthomi on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct OrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
